f4d8608fc1f1b5d202fe28ff2ced793775dddeb5
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-01-03 17:10:44 +0000

    anchor links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,26 @@
 import { Link } from 'gatsby';
+import { AnchorLink } from 'gatsby-plugin-anchor-links';
 import React from 'react';
 
 export const Nav = () => {
   return (
     <nav>
       <ul>
-        <li>Introduction</li>
-        <li>About</li>
-        <li>Expertise</li>
-        <li>Founder</li>
-        <li>FAQ</li>
+        <li>
+          <AnchorLink to='#introduction'>Introduction</AnchorLink>
+        </li>
+        <li>
+          <AnchorLink to='#about'>About</AnchorLink>
+        </li>
+        <li>
+          <AnchorLink to='#expertise'>Expertise</AnchorLink>
+        </li>
+        <li>
+          <AnchorLink to='#founder'>Founder</AnchorLink>
+        </li>
+        <li>
+          <AnchorLink to='#faq'>FAQ</AnchorLink>
+        </li>
       </ul>
     </nav>
   );
