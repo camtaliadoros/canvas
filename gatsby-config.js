eb80b31bd2daa16cba0de88f940275ff4e145ed3
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-01-10 11:54:35 +0000

    fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,37 @@
 /**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  siteMetadata: {
+    title: `Canvas Philanthropy 2024`,
+    siteUrl: `https://canvasphilanthropy.com/`,
+    description: `We support inspirational people instigate meaningful and powerful change in the world. Established in 2015, we shape, launch and grow philanthropic and corporate socially responsible initiatives.`,
+  },
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-anchor-links`,
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
+        web: [
+          {
+            name: `ABCNormal Book`,
+            file: `https://db.onlinewebfonts.com/c/bad5d85dec62adc89f2f88300b74dd63?family=ABC+Normal+Book`,
+          },
+          {
+            name: `Ivy Ora`,
+            file: `https://use.typekit.net/emz5qfb.css`,
+          },
+        ],
+      },
+    },
   ],
 };
