d3d8a5b16fef1015d0586c8bdbfb2419aa9816e5
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-01-10 14:27:02 +0000

    about styling

--- src/components/About.js
+++ src/components/About.js
@@ -2,34 +2,25 @@ import React from 'react';
 
 export const About = () => {
   return (
-    <section id='about'>
-      <h1>About</h1>
-      <h2>A Space to Care</h2>
-      <h4>
+    <section className='white-container-section' id='about'>
+      <h1 className='dark'>About</h1>
+      <h2 className='dark'>A Space to Care</h2>
+      <h4 className='dark'>
         We have the priviledge of working with client of all shapes and sizes,
         yet all united in their purpose to enhance the world we live in.
       </h4>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ultrices neque
-        ornare aenean euismod. Ac turpis egestas sed tempus. Dignissim convallis
-        aenean et tortor. Gravida in fermentum et sollicitudin ac orci phasellus
-        egestas. Commodo nulla facilisi nullam vehicula ipsum a arcu. Eleifend
-        donec pretium vulputate sapien nec. Sed arcu non odio euismod. Urna et
-        pharetra pharetra massa massa ultricies mi quis. At volutpat diam ut
-        venenatis. Interdum varius sit amet mattis vulputate enim nulla aliquet
-        porttitor. Lectus magna fringilla urna porttitor rhoncus dolor purus.
-        Feugiat nisl pretium fusce id velit ut tortor pretium. Orci phasellus
-        egestas tellus rutrum tellus pellentesque. Nec nam aliquam sem et
-        tortor. A iaculis at erat pellentesque adipiscing. Pellentesque sit amet
-        porttitor eget dolor. Scelerisque varius morbi enim nunc faucibus. Lorem
-        sed risus ultricies tristique nulla aliquet enim tortor. Sed turpis
-        tincidunt id aliquet risus feugiat in ante metus. Risus sed vulputate
-        odio ut enim blandit volutpat maecenas. Sed tempus urna et pharetra
-        pharetra massa. Quis commodo odio aenean sed. Pharetra pharetra massa
-        massa ultricies mi quis hendrerit dolor magna. Congue eu consequat ac
-        felis donec. Sit amet purus gravida quis blandit turpis. Fringilla
-        phasellus faucibus scelerisque eleifend. Nulla pharetra diam sit amet.
+      <p className='dark'>
+        Canvas was founded in 2015 by Alexandra Taliadoros following a career
+        leading charitable and philanthropic initiatives for individuals,
+        families and businesses across both international and domestic
+        landscapes.
+      </p>
+      <p className='dark'>
+        Through a menu of services, from charity establishment to charitable
+        foundation direction; corporate social responsibility consultancy to
+        governance and compliance controls (and everything in between) we
+        support those who wish to make a difference, create meaningful and
+        powerful change in the world.
       </p>
     </section>
   );
