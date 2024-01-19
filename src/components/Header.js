840936214ff18feb185642d5b1d8895013919465
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-01-19 22:23:21 +0000

    logo anchor link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
 import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
 import * as styles from '../styles/header.module.css';
+import { AnchorLink } from 'gatsby-plugin-anchor-links';
 
 export const Header = () => {
   return (
     <>
       <div className={styles.header}>
-        <StaticImage
-          src='../assets/canvas-logo.png'
-          alt='canvas-logo'
-          placeholder='blurred'
-          loading='eager'
-          width={60}
-        />
+        <AnchorLink to='#introduction'>
+          <StaticImage
+            src='../assets/canvas-logo.png'
+            alt='canvas-logo'
+            placeholder='blurred'
+            loading='eager'
+            width={60}
+          />
+        </AnchorLink>
       </div>
       <div className={styles.contactLinkContainer}>
         <a href='mailto:info@canvasphilanthropy.com'>Contact us</a>
