463c6bfaefff9014ec8396f2b7a5aefa8d410d3e
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-01-19 15:14:58 +0000

    faq answer transition

--- src/components/FAQDropdown.js
+++ src/components/FAQDropdown.js
@@ -15,12 +15,14 @@ export const FaqDropdown = ({ content }) => {
       <button className={styles.questionContainer} onClick={handleClick}>
         <p className='large'>{question}</p>
         <div
-          className={`${styles.chevron} ${
-            isOpen ? styles.openChevron : styles.closedChevron
-          }`}
+          className={`${styles.chevron} ${isOpen ? styles.openChevron : null}`}
         ></div>
       </button>
-      <div className={styles.answerContainer}>
+      <div
+        className={`${styles.answerContainer} ${
+          isOpen ? styles.openAnswer : null
+        }`}
+      >
         {answer.map((paragraph) => (
           <p>{paragraph}</p>
         ))}
