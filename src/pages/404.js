c12197e4d6d5907d44129dad91e3110bf114c41c
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-01-19 22:01:36 +0000

    404 page

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
+import * as styles from '../styles/404.module.css';
+import { Link } from 'gatsby';
+
+export default function notFound() {
+  return (
+    <div className={styles.wrapper}>
+      <Header />
+      <main className={styles.mainContainer}>
+        <Link to='/'>
+          <h1>Go back home</h1>
+        </Link>
+        <h2>This page doesn't exist</h2>
+      </main>
+      <Footer />
+    </div>
+  );
+}
