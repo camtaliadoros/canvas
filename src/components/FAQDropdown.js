399412690e13c1f35bad5ce8f361c77917f5a943
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-01-19 14:51:38 +0000

    chevron styling

--- src/components/FAQDropdown.js
+++ src/components/FAQDropdown.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as styles from '../styles/faq.module.css';
 
 export const FaqDropdown = ({ content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const { question, answer } = content;
 
-  const handleClick = () => {};
+  const handleClick = () => {
+    setIsOpen((prevState) => !prevState);
+  };
 
   return (
     <div className={styles.container}>
       <button className={styles.questionContainer} onClick={handleClick}>
         <p className='large'>{question}</p>
-        <div className={styles.chevron}></div>
+        <div
+          className={`${styles.chevron} ${
+            isOpen ? styles.openChevron : styles.closedChevron
+          }`}
+        ></div>
       </button>
       <div className={styles.answerContainer}>
         {answer.map((paragraph) => (
