cb2288c9bdb5892b99bd068aca4b1cb73ca9f471
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-02-07 12:47:09 +0000

    video format change

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as styles from '../styles/introduction.module.css';
 import { Testimonials } from './Testimonials';
+import Sphere from '../assets/Ball-with-Feather.webm';
 // import Sphere from '../assets/sphere.mp4';
-import Sphere from '../assets/sphere.mp4';
 
 export const Introduction = () => {
   return (
@@ -17,7 +17,14 @@ export const Introduction = () => {
         </p>
       </div>
       <div className={styles.videoContainer}>
-        <video autoPlay loop muted playsInline className={styles.video}>
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          disableRemotePlayback
+          className={styles.video}
+        >
           <source src={Sphere} type='video/mp4' />
         </video>
       </div>
